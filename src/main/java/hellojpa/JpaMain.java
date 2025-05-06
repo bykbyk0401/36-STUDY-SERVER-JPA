794b2a4027147e139820846c9ce498bc5a73709c
bykbyk0401 <bykbyk0401@gmail.com>
2025-05-06 16:36:52 +0900

    init: JpaMain 클래스 생성

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,7 +1,15 @@
 package hellojpa;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class JpaMain {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        //code
+        em.close();
+        emf.close();
     }
 }
