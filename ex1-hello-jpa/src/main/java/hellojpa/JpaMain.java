f61ecc359b1ee23bdddf9c2762e54fc205c7a452
bykbyk0401 <bykbyk0401@gmail.com>
2025-05-27 04:35:37 +0900

    feat: 타입 비교 예제

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,19 +15,36 @@ public class JpaMain {
 
         try {
 
-            // getReference 예제
-            Member member = new Member();
-            member.setUsername("hello");
+//            // getReference 예제
+//            Member member = new Member();
+//            member.setUsername("hello");
+//
+//            em.persist(member);
+//
+//            em.flush();
+//            em.clear();
+//
+////            Member findMember = em.find(Member.class, member.getId());
+//            Member findMember = em.getReference(Member.class, member.getId());
+//            System.out.println("findMember.id = " + findMember.getId());
+//            System.out.println("findMember.username = " + findMember.getUsername());
 
-            em.persist(member);
+            // 타입 비교
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-//            Member findMember = em.find(Member.class, member.getId());
-            Member findMember = em.getReference(Member.class, member.getId());
-            System.out.println("findMember.id = " + findMember.getId());
-            System.out.println("findMember.username = " + findMember.getUsername());
+            Member m1 = em.find(Member.class, member1.getId());
+            Member m2 = em.getReference(Member.class, member2.getId());
+
+            System.out.println("m1 == m2 : " + (m1.getClass() == m2.getClass())); // false
 
             tx.commit();
         } catch (Exception e) {
